2b6c8098d21fbc3686c3b0ea35bd8972c57e0b2a
Autumn Waller <@autumnwaller@proton.me>
2023-11-09 22:51:01 +0000

    Website structure beginning

--- src/tsx/components/gridButton.tsx
+++ src/tsx/components/gridButton.tsx
@@ -1,10 +1,18 @@
-import { Component } from "preact";
+import { Component} from "preact";
 import "../../css/gridButton.css"
 
-export class GridButton extends Component<{}, {}>{
-    render(){
-        return <div className={"GridButton_root"}>
+export type GridButtonProps = 
+{
+    title: string,
+    subtitle: string,
+    backgroundImage: string,
+}
 
+export class GridButton extends Component<GridButtonProps, {}>{
+    render(props: GridButtonProps){
+        return <div className={"GridButton_root"}>
+            <div className={"GridButton_title"}>{props.title}</div>
+            <div className={"GridButton_subtitle"}>{props.subtitle}</div>
         </div>
     }
 }
--- src/tsx/page.tsx
+++ src/tsx/page.tsx
@@ -1,16 +1,43 @@
 import { Component } from "preact";
 import '../css/page.css'
-import { GridButton } from "./components/gridButton";
+import { ProjectContainer } from "./components/projectContainer";
 
 export class Page extends Component<{}, {}> {
     
     render()
     {
         return <div className={"Page_root"}>
-            <div className={"Page_title"}>
-                Placeholder Title
+            <div className={"Page_header"}>
+                <div className={"Page_headerBox"}>
+                    <div className={"Page_headerText h3"}>
+                        Autumn Waller
+                    </div>
+                    <div className={"Page_headerText"}>About Me</div>
+                    <div className={"Page_headerText"}>Projects</div>
+                    <div className={"Page_headerText"}>CV</div>
+                    <div className={"Page_headerText"}>Contact</div>
+                </div>
             </div>
-            <GridButton/>
+
+            <div className={"Page_heading"}>About Me</div>
+            <div className={"Page_textBox"}>
+                <div className={"Page_aboutMe"}>
+                I am Autumn, a programmer with experience in various areas within games. 
+                I have been making games since I was in secondary school and discovered how to use Game Maker. Eventually, I went to study Game Design in college where
+                I self-taught programming C# through Unity which led to me publishing my first two games on the Google Play Store: Dead of Knight and Bun Run.
+                For both of these games, I worked with a friend, where he created the art and 3d models and I created the code.
+                Following college, I was accepted into Staffordshire University where I achieved a first-class degree in Computer Games Programming. 
+                Through University I learned C++, DirectX11, OpenGL, among other things. To be rewritten.
+                </div>
+            </div>
+            <div className={"Page_heading"}>Projects</div>
+            <ProjectContainer
+                title={"[Redacted]"}
+                subtitle={""}
+                description={""}
+                mainImage={"../assets/ror.jpg"}
+            />
+
         </div>
     }
 }
